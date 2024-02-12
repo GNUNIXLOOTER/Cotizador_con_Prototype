a1c78e2f4edc426fa5b5ea3da3b26ddd9c314b3e
GNUNIXLOOTER <andresparradoparrado@gmail.com>
2024-02-12 15:26:29 -0500

    se actuyaliza el codigo para mas aprendizaje

--- js/app.js
+++ js/app.js
@@ -4,13 +4,19 @@ function Seguro(marca, anio, tipo) {
      this.anio = anio;
      this.tipo = tipo;
 }
+
+
 Seguro.prototype.cotizarSeguro = function() {
      /*
           1 = americano 1.15
           2 = asiatico 1.05
           3 = europeo 1.35
      */
+
+     // Se le coloca un let por que la variable no va a tener valor de momento
      let cantidad;
+
+     // Se le coloca un const por que esta si tiene que tener valor
      const base = 2000;
 
      switch(this.marca){
@@ -25,13 +31,14 @@ Seguro.prototype.cotizarSeguro = function() {
                break;
      }
 
-     // Leer el año
+     // Leer el año actual y le resta this.anio
      const diferencia = new Date().getFullYear() - this.anio;
+
      // Cada año de diferencia hay que reducir 3% el valor del seguro
      cantidad -= ((diferencia * 3) * cantidad) / 100;
      /*
-          Si el seguro es básico se múltiplica por 30% mas
-          Si el seguro es completo 50% mas
+          Si el seguro es básico se múltiplica por un 30% mas
+          Si el seguro es completo un 50% mas
      */
     if(this.tipo === 'basico') {
          cantidad *= 1.30;
@@ -44,21 +51,29 @@ Seguro.prototype.cotizarSeguro = function() {
 }
 
 // Todo lo que se muestra
+// La interfaz de usuario no requiere datos, esos datos se llenan de acuerdo a lo que vaya sucediendo en la aplicacion.
+// por tal este objeto va estar vacio
 function Interfaz() {}
 
-// Mensaje que se imprime en el HTML
+
+// Mensaje que se imprime en el HTML, en este caso muestra un mensaje de error y mensaje de exito.
 Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
      const div = document.createElement('div');
 
+     //validamos si el mensaje es error o correcto
      if(tipo === 'error') {
           div.classList.add('mensaje','error');
      } else {
           div.classList.add('mensaje','correcto');
      }
+
      div.classList.add('mt-10');
      div.innerHTML = `${mensaje}`;
-     formulario.insertBefore(div, document.querySelector('#resultado')); // Nuevo Nodo y referencia... // Si la referencia no existe se añadira al final
 
+      // cuando usamos: insertBefore es Nuevo Nodo y referencia... // Si la referencia no existe se añadira al final
+     formulario.insertBefore(div, document.querySelector('#resultado'));
+
+     //setTimeout es para que el mensaje despues de 3 segundos no se muestra mas
      setTimeout( () =>  {
           document.querySelector('.mensaje').remove();
      }, 3000);
@@ -66,8 +81,10 @@ Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
 
 // Imprime el resultado de la cotización
 Interfaz.prototype.mostrarResultado = function(seguro, total) {
+
      const resultado = document.querySelector('#resultado');
      let marca;
+
      switch(seguro.marca) {
           case '1':
                marca = 'Americano';
@@ -79,10 +96,12 @@ Interfaz.prototype.mostrarResultado = function(seguro, total) {
                marca = 'Europeo';
                break;
      }
+
      // Crear un div
      const div = document.createElement('div');
-     div.classList.add('mt-10')
-     // Insertar la informacion
+     div.classList.add('mt-10');
+
+     // Insertar la informacion, el innerHTML modifica el html y textContent cuando no tiene html
      div.innerHTML = `
           <p class='header'>Tu Resumen: </p>
           <p class="font-bold">Marca: <span class="font-normal"> ${marca} </span> </p>
@@ -91,8 +110,10 @@ Interfaz.prototype.mostrarResultado = function(seguro, total) {
           <p class="font-bold"> Total: <span class="font-normal"> $ ${total} </span> </p>
      `;
 
+
      const spinner = document.querySelector('#cargando');
      spinner.style.display = 'block';
+
      setTimeout( () =>  {
           spinner.style.display = 'none';
           resultado.appendChild(div);
@@ -100,10 +121,13 @@ Interfaz.prototype.mostrarResultado = function(seguro, total) {
      
 }
 
+
+// Este codigo llena las opciones de los años
 Interfaz.prototype.llenarOpciones = function () {
      const max = new Date().getFullYear(),
-          min = max - 20;
+          min = max - 20;// En este caso muestra 20 años nada mas
 
+     //Este codigo muestra los años del actual al antiguo
      const selectAnios = document.querySelector('#year');
      for(let i = max; i > min; i--) {
           let option = document.createElement('option');
@@ -113,12 +137,14 @@ Interfaz.prototype.llenarOpciones = function () {
      }   
 }
 
+
 // Crear instancia de Interfaz
 const interfaz = new Interfaz();
 
 document.addEventListener('DOMContentLoaded', () => {
-     interfaz.llenarOpciones()
+     interfaz.llenarOpciones(); //Llena el select con los años
 });
+
 
 // DOM Operaciones
 const formulario = document.querySelector('#cotizar-seguro');
@@ -126,32 +152,42 @@ const formulario = document.querySelector('#cotizar-seguro');
 formulario.addEventListener('submit', e =>  {
      e.preventDefault();
 
-     // leer la marca seleccionada del select
+     // leer la marca seleccionada del select y su valor con .value
      const marca = document.querySelector('#marca').value;
 
      // leer el año seleccionado del <select>
      const year = document.querySelector('#year').value
 
      // lee el valor del radio button
+     // este input[name="tipo"] es un selector de css y en este caso nos interesa el que el usuario le dio al radio check
+     // osea :checked
+     // de esta forma se lee el input de tipo radio y .value para leer su valor
      const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
  
 
-     // Revisamos que los campos no esten vacios
+     // Revisamos que los campos no esten vacios validando por campo
      if(marca === '' || year === '' || tipo === '') {
+
           // Interfaz imprimiendo un error
+          //aqui se muestra el mensaje de error por que el formulario no esta lleno completamente
           interfaz.mostrarMensaje('Faltan datos, revisar el formulario y prueba de nuevo', 'error');
      } else {
+
           // Limpiar resultados anteriores
           const resultados = document.querySelector('#resultado div');
+
+          // Si resultados no es igual a null
           if(resultados != null) {
                resultados.remove();
           }
 
           // Instanciar seguro y mostrar interfaz
           const seguro = new Seguro(marca, year, tipo);
+
           // Cotizar el seguro
           const cantidad = seguro.cotizarSeguro();
+
           // Mostrar el resultado
           interfaz.mostrarResultado(seguro, cantidad);
           interfaz.mostrarMensaje('Cotizando...', 'exito');
